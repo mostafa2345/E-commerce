3b57cc810a102c80f848f19f4394505bcc924620
mostafa2345 <74478034+mostafa2345@users.noreply.github.com>
2025-11-14 17:41:01 +0200

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,17 +5,6 @@ import path from 'path';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-    resolve: {
-    alias: {
-      react: path.resolve('./node_modules/react'),
-      'react-dom': path.resolve('./node_modules/react-dom'),
-    },
-  },
-  server:{
-    proxy:{
-      '/api':{
-        target:'http://localhost:5000'
-      }
-    }
-  }
+ 
+
 })
