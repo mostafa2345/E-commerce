5472f119de7ecd9fdf7d044bcad31bc101458cbe
mostafa2345 <mostafawaheed680@gmail.com>
2025-11-14 15:47:13 +0200

    project completed

---
+++ backend/routes/analytic.route.js
@@ -0,0 +1,33 @@
+import express from 'express'
+import { adminRoute, protectRoute } from '../middlewares/auth.middleware.js'
+import { getAnalyticsData, getDailySalesData } from '../controllers/analytic.controller.js'
+
+
+const router=express.Router()
+
+router.get('/',protectRoute,adminRoute,async(req,res)=>{
+    try {
+        const analyticsData=await getAnalyticsData()
+       
+        const endDate=new Date()
+         const startDate=new Date(endDate.getTime()-7*24*60*60*1000);
+
+         const dailySalesData=await getDailySalesData(startDate,endDate)
+
+        
+
+        
+         res.json({analyticsData,dailySalesData})
+
+    } catch (error) {
+        console.log('error in the analytic route',error.message)
+        res.status(500).json({message:'server error',error:error.message})
+    }
+})
+
+
+
+
+export default router
+
+
---
+++ frontend/src/stores/userUserStore.js
@@ -0,0 +1,109 @@
+import { create } from "zustand";
+import axios from '../lib/axios'
+import {toast} from 'react-hot-toast'
+
+
+
+
+
+export const useUserStore=create((set,get)=>({
+user:null,
+loading:false,
+checkingAuth:true,
+
+signup:async({name,email,password,confirmPassword})=>{
+    set({loading:true})
+    if(password!==confirmPassword){
+          set({loading:false})
+      return  toast.error('Passwrod not match')
+    }
+    try {
+        const res=await axios.post('/auth/register',{name,email,password})
+        set({user:res.data,loading:false})
+
+    } catch (error) {
+          set({loading:false})
+          toast.error(error.response.data.message||'An error occured')
+    }
+
+},
+login:async(email,password)=>{
+set({loading:true})
+try {
+    const res=await axios.post('/auth/login',{email,password})
+    set({user:res.data,loading:false})
+} catch (error) {
+     set({loading:false})
+          toast.error(error.response.data.message||'An error occured')
+}
+} ,
+
+
+
+checkAuth:async()=>{
+    set({checkingAuth:true})
+    try {
+        const res =await axios.get('/auth/profile')
+       console.log(res)
+        set({user:res.data,checkingAuth:false})
+    } catch  {
+        set({checkingAuth:false,user:null})
+         
+    }
+},
+logout:async()=>{
+ 
+    try {
+          await axios.post('/auth/logout')
+             set({user:null})
+    } catch (error) {
+      
+          toast.error(error.response.data.message||'An error occured')
+    }
+  
+
+},
+refreshToken:async()=>{
+    console.log('start refrsh token')
+    if(get().checkingAuth) return refreshPromise
+   
+    set({checkingAuth:true})
+    try {
+   const res= await axios.post('/auth/refresh-token')
+   set({checkingAuth:false})
+        console.log(res)
+    return res.data
+   
+    } catch (error) {
+        set({user:null,checkingAuth:false})
+        throw error
+    }
+}
+}))
+let refreshPromise=null;
+axios.interceptors.response.use(
+    (res)=>res,
+    async(error)=>{
+        const originalRequest=error.config;
+        if(error.response?.status===401 && !originalRequest._retry){
+            originalRequest._retry=true
+            try {
+                if(!refreshPromise){
+                    refreshPromise=useUserStore.getState().refreshToken()
+                    
+                }
+                
+                await refreshPromise
+                refreshPromise=null
+                return axios(originalRequest)
+            } catch (refreshError) {
+                useUserStore.getState().logout()
+                return Promise.reject(refreshError)
+            }
+
+        }
+        return Promise.reject(error)
+    }
+)
+//TODO:Implement the axios interceptor for refreshing access token
+ 
