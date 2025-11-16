45c8d9decfcb48b3959a755d25cf7585d6f54438
mostafa2345 <mostafawaheed680@gmail.com>
2025-11-16 20:52:33 +0200

    fix: make manual chunks to improve performance

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,9 +2,22 @@ import { defineConfig } from 'vite'
 import react from "@vitejs/plugin-react";
 import tailwindcss from '@tailwindcss/vite'
 
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [ react(),tailwindcss()],
+  build:{
+    rollupOptions:{
+      output:{
+        manualChunks:{
+          react:['react','react-dom'],
+          stripe:['@stripe/react-stripe-js', '@stripe/stripe-js'],
+          charts:['recharts'],
+          motion:['framer-motion', 'motion']
+        }
+      }
+    }
+  },
   server:{
     proxy:{
       '/api':{
