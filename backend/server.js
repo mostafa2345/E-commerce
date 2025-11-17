9ad58ed34daccfb3ba9a2351c27e09217abe98ab
mostafa2345 <mostafawaheed680@gmail.com>
2025-11-17 20:34:47 +0200

    changes

--- backend/server.js
+++ backend/server.js
@@ -8,12 +8,12 @@ import couponRoutes from '../backend/routes/coupon.route.js'
 import paymentRoutes from '../backend/routes/payment.route.js'
 import analyticsRoutes from '../backend/routes/analytic.route.js'
 import cookieParser from 'cookie-parser'
-import path from 'path'
+
 dotenv.config()
 const app=express()
 
 const PORT=process.env.PORT||5000
-const __dirname=path.resolve()
+
 app.use(express.json({ limit: "10mb" }))
 app.use(cookieParser())
 app.use('/api/auth',authRoutes)
@@ -22,12 +22,7 @@ app.use('/api/cart',cartRoutes)
 app.use('/api/coupons',couponRoutes)
 app.use('/api/payments',paymentRoutes)
 app.use('/api/analytics',analyticsRoutes)
-if(process.env.NODE_ENV==='production'){
-    app.use(express.static(path.join(__dirname,'frontend','dist')))
-    app.get('/*path',(req,res)=>{
-        res.sendFile(path.resolve(__dirname,'frontend','dist','index.html'))
-    })
-}
+
 app.listen(PORT,()=>{
     console.log(`server is running on ${PORT}` )
     connectDB()
